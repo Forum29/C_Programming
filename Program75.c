8b7465d20ad52a7fd58c843bd53954795d985646
Forum29 <forumtailor866@gmail.com>
2025-10-03 16:58:01 +0530

    Create Program75.c

---
+++ Program75.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    float side, area;
+    printf("Enter side of square: ");
+    scanf("%f", &side);
+    area = side * side;
+    printf("Area of Square = %.2f\n", area);
+    return 0;
+}
