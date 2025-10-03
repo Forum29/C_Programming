88d52f3bfec5a4c5789a708626bc45ebb71e79e5
Forum29 <forumtailor866@gmail.com>
2025-10-03 17:06:34 +0530

    Create Program83.c

---
+++ Program83.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    float price, discount, finalPrice;
+    printf("Enter original price: ");
+    scanf("%f", &price);
+    printf("Enter discount percentage: ");
+    scanf("%f", &discount);
+    finalPrice = price - (price * discount / 100);
+    printf("Final Price after discount = %.2f\n", finalPrice);
+    return 0;
+}
