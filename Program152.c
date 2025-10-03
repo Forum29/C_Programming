526c793e8e530f44482702ca1f7d167c1b986dc6
Forum29 <forumtailor866@gmail.com>
2025-10-03 17:51:10 +0530

    Create Program152.c

---
+++ Program152.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n, i, marks, total = 0;
+    float percentage;
+    printf("Enter number of subjects: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++) {
+        printf("Enter marks for subject %d: ", i);
+        scanf("%d", &marks);
+        total += marks;
+    }
+    percentage = (float)total / n;
+    printf("Total = %d, Percentage = %.2f%%\n", total, percentage);
+    return 0;
+}
