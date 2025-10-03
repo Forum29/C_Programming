cdcf7ebc1aa16e4e9bb46783085394d567c84a4c
Forum29 <forumtailor866@gmail.com>
2025-10-03 16:04:25 +0530

    Create Program24.c

---
+++ Program24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    float n, root;
+    printf("Enter a number: ");
+    scanf("%f", &n);
+    root = sqrt(n);
+    printf("Square Root = %.2f\n", root);
+    return 0;
+}
