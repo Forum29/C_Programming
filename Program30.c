9f2fecc5bb3483e7702d6192e70d0bc0146cb6ea
Forum29 <forumtailor866@gmail.com>
2025-10-03 16:23:29 +0530

    Create Program30.c

---
+++ Program30.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int mins;
+    float hrs;
+    printf("Enter minutes: ");
+    scanf("%d", &mins);
+    hrs = mins / 60.0;
+    printf("%d minutes = %.2f hours\n", mins, hrs);
+    return 0;
+}
