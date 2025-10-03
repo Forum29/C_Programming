03c907d4a4f09c91cb67d8b61bddff30aedd81aa
Forum29 <forumtailor866@gmail.com>
2025-10-03 17:59:23 +0530

    Create Program165.c

---
+++ Program165.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int n, i, num, sum = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++) {
+        printf("Enter number %d: ", i);
+        scanf("%d", &num);
+        if(num < 0)
+            sum += num;
+    }
+    printf("Sum of negative numbers = %d\n", sum);
+    return 0;
+}
