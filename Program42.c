0a6674a9cf5ebdca09829410c2b4965e236eaf90
Forum29 <forumtailor866@gmail.com>
2025-10-03 16:35:26 +0530

    Create Program42.c

---
+++ Program42.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n > 0)
+        printf("%d is Positive\n", n);
+    else if(n < 0)
+        printf("%d is Negative\n", n);
+    else
+        printf("Number is Zero\n");
+    return 0;
+}
