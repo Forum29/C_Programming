31002c8584f221cd1f74c9f1b540c7bbb79698ae
Forum29 <forumtailor866@gmail.com>
2025-10-03 15:55:59 +0530

    Create Program16.c

---
+++ Program16.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    float km, miles;
+    printf("Enter distance in kilometers: ");
+    scanf("%f", &km);
+    miles = km * 0.621371;
+    printf("Distance in miles = %.2f\n", miles);
+    return 0;
+}
