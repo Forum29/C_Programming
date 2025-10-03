1c29bd7bd0dfc93b244a9982a2e0c71b76dfe1e8
Forum29 <forumtailor866@gmail.com>
2025-10-03 17:31:52 +0530

    Create Program121.c

---
+++ Program121.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    float a, b, div;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+    if(b != 0) {
+        div = a / b;
+        printf("Division = %.2f\n", div);
+    } else {
+        printf("Error: Division by zero is not allowed!\n");
+    }
+    return 0;
+}
