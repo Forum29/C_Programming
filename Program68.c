4f0c4dae581563adc9ae2206942de4b26170ab56
Forum29 <forumtailor866@gmail.com>
2025-10-03 16:54:17 +0530

    Create Program68.c

---
+++ Program68.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int sec, min, hrs;
+    printf("Enter time in seconds: ");
+    scanf("%d", &sec);
+    hrs = sec / 3600;
+    min = (sec % 3600) / 60;
+    sec = sec % 60;
+    printf("Time = %d hours %d minutes %d seconds\n", hrs, min, sec);
+    return 0;
+}
