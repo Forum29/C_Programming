ba4091e7c315b393ff4c31c003a42348b6a25efb
Forum29 <forumtailor866@gmail.com>
2025-10-03 17:33:43 +0530

    Create Program124.c

---
+++ Program124.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int a, b, c, d, e, min;
+    printf("Enter 5 numbers: ");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    min = a;
+    if(b < min) min = b;
+    if(c < min) min = c;
+    if(d < min) min = d;
+    if(e < min) min = e;
+    printf("Minimum = %d\n", min);
+    return 0;
+}
