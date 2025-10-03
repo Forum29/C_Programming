2e817519b1df7953f131fba13bd426875b4c6430
Forum29 <forumtailor866@gmail.com>
2025-10-03 17:15:24 +0530

    Create Program100.c

---
+++ Program100.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int m1, m2, m3, m4, m5, total;
+    float percentage;
+    printf("Enter marks in 5 subjects: ");
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+    total = m1 + m2 + m3 + m4 + m5;
+    percentage = (total / 500.0) * 100;
+    printf("Total = %d, Percentage = %.2f%%\n", total, percentage);
+    return 0;
+}
