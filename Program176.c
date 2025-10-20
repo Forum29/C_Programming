6505966dcdcdc487b76b5e58c4f238f7ed4f3e3e
Forum29 <forumtailor866@gmail.com>
2025-10-20 22:04:01 +0530

    Create Program176.c

---
+++ Program176.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    printf("Enter marks (0-100): ");
+    scanf("%d", &marks);
+
+    switch(marks / 10) {
+        case 10:
+        case 9: printf("Grade: A"); break;
+        case 8: printf("Grade: B"); break;
+        case 7: printf("Grade: C"); break;
+        case 6: printf("Grade: D"); break;
+        default: printf("Grade: Fail");
+    }
+    return 0;
+}
