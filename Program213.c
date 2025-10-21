9325b47790adc9176a2feb903cc359e898aef0b3
Forum29 <forumtailor866@gmail.com>
2025-10-22 00:15:07 +0530

    Add prime number checking function

---
+++ Program213.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i <= n / 2; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+int main() {
+    printf("%d", isPrime(7));
+    return 0;
+}
