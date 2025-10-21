32010af1860ec9db8ed5d3382af2c24f5ca96d5c
Forum29 <forumtailor866@gmail.com>
2025-10-22 00:12:29 +0530

    Implement Fibonacci sequence in Program211.c

---
+++ Program211.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void fibonacci(int n) {
+    int a = 0, b = 1, c;
+    printf("%d %d ", a, b);
+    for(int i = 2; i < n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+}
+
+int main() {
+    fibonacci(10);
+    return 0;
+}
