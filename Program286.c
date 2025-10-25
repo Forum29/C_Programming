0bfa6959e2ae978be3c6bdca2e9fb5ee2a882f93
Forum29 <forumtailor866@gmail.com>
2025-10-25 13:19:36 +0530

    Create Program286.c

---
+++ Program286.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void decToBin(int n){
+    int bin[32],i=0;
+    while(n>0){ bin[i++]=n%2; n/=2; }
+    for(int j=i-1;j>=0;j--) printf("%d",bin[j]);
+}
+int main(){ int n; scanf("%d",&n); decToBin(n); }
